7aa1e795df8a57f07585a1f63d9892c18607e082
Code Nation <codenation@Codes-MacBook-Pro.local>
2019-11-29 11:37:49 +0000

    made css file and moved styling from html

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 let playerScore = 0
+let player2score = 0
 
 document.getElementById("button").addEventListener("click", () => {
     let random = Math.floor(Math.random() * 6) +1
     const dices = document.getElementById("button").textContent = 'roll';
     const images = document.getElementById("image");
     playerScore = playerScore + random;
-    document.getElementById(`dice`).innerHTML = `score = ${playerScore}`;
+    document.getElementById(`player1score`).innerHTML = `score = ${playerScore}`;
     images.src = `image${random}.png`;
 
     if (random == 1 && playerScore < 20) {
