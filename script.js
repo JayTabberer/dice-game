686d10b6f2116587b247dfd5f7d814b0fbe59093
Code Nation <codenation@Codes-MacBook-Pro.local>
2019-11-29 13:49:16 +0000

    cleaned up syntax and added a funtion to clear the score

--- script.js
+++ script.js
@@ -2,24 +2,37 @@ let playerScore = 0
 let player2score = 0
 
 document.getElementById("button").addEventListener("click", () => {
-    let random = Math.floor(Math.random() * 6) +1
-    const dices = document.getElementById("button").textContent = 'roll';
-    const images = document.getElementById("image");
-    playerScore = playerScore + random;
-    document.getElementById(`player1score`).innerHTML = `score = ${playerScore}`;
-    images.src = `image${random}.png`;
-
-    if (random == 1 && playerScore < 20) {
-        document.getElementById('dice').innerHTML += `You lose try again`;
-        button.textContent = 'try again'
-        total = 0;
-    } else if (playerScore >= 20) {
-        document.getElementById('dice').innerHTML += `You win`;
-        button.textContent = 'play again'
-        total = 0;
-    }
-
+    diceLogic(1);
 });
 
+document.getElementById("button2").addEventListener('click', () => {
+    diceLogic(2);
+});
 
+let diceLogic = (playerNumber) => {
+    let random = Math.floor(Math.random() * 6) +1
+    const images = document.getElementById(`image${playerNumber}`);
+    if (playerNumber == 1) {
+        playerScore = playerScore + random;
+        document.getElementById(`player${playerNumber}score`).innerHTML = `score = ${playerScore}`;
+    } else {
+        player2score = player2score + random;
+        document.getElementById(`player${playerNumber}score`).innerHTML = `score = ${player2score}`;
+    }
+    images.src = `image${random}.png`;
 
+    if (random == 1) {
+        document.getElementById(`player${playerNumber}score`).innerHTML += `You lose try again`;
+        clearScore()
+        // button.textContent = 'try again'   
+    } else if (playerScore >= 21) {
+        document.getElementById(`player${playerNumber}score`).innerHTML += `You win`;
+        clearScore()
+        // button.textContent = 'play again'
+    }
+}
+
+let clearScore = () => {
+    playerScore = 0
+    player2score = 0
+}
