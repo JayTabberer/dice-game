f8420a914f7426b8a8f12fd2ab355e925a76c5c2
Code Nation <codenation@Codes-MacBook-Pro.local>
2019-11-29 10:41:53 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+let playerScore = 0
+
+document.getElementById("button").addEventListener("click", () => {
+    let random = Math.floor(Math.random() * 6) +1
+    const dices = document.getElementById("button").textContent = 'roll';
+    const images = document.getElementById("image");
+    playerScore = playerScore + random;
+    document.getElementById(`dice`).innerHTML = `score = ${playerScore}`;
+    images.src = `image${random}.png`;
+
+    if (random == 1 && playerScore < 20) {
+        document.getElementById('dice').innerHTML += `You lose try again`;
+        button.textContent = 'try again'
+        total = 0;
+    } else if (playerScore >= 20) {
+        document.getElementById('dice').innerHTML += `You win`;
+        button.textContent = 'play again'
+        total = 0;
+    }
+
+});
+
+
+
